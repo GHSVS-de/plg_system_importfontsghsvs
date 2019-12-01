b3ce9d75439b95ddf4e958b16ac7122186cc4218
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2019-12-01 16:57:39 +0100

    importfontsghsvs V 2019.12.01 (small changes and additions)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # plg_system_importfontsghsvs
 
+- Joomla-System-Plugin.
+- Import and save G WebFonts and browser dependent CSS via server IP and not page visitor IP.
+- Joomla's /media/ folder must be writable for PHP (Joomla) to add and fill folder /fontsghsvs/ on-the-fly.
+
 ## Work in progress!
--- importfontsghsvs.php
+++ importfontsghsvs.php
@@ -367,7 +367,7 @@ class PlgSystemImportFontsGhsvs extends CMSPlugin
 				$file        = __DIR__ . '/myforms/' . $file . '.xml';
 				
 				if (
-					empty($subformData) || !is_object($subformData)
+					!is_object($subformData) || !count(get_object_vars($subformData))
 					|| !is_file($file) 
 				)
 				{
