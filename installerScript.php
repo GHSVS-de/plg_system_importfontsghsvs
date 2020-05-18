55237fd0a5b03516a0d1455fc652f270e597f313
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2020-05-18 17:12:36 +0200

    v 2020.05.18

--- installerScript.php
+++ installerScript.php
@@ -40,49 +40,52 @@ class plgSystemImportfontsGhsvsInstallerScript extends InstallerScript
 
 	public function preflight($type, $parent)
 	{
-		if (!parent::preflight($type, $parent))
-		{
-			return false;
-		}
-
 		$manifest = @$parent->getManifest();
 		
 		if ($manifest instanceof SimpleXMLElement)
 		{
-			$minimumPhp = trim((string) $manifest->minimumPhp);
-			$minimumJoomla = trim((string) $manifest->minimumJoomla);
-			
-			// Custom
-			$maximumPhp = trim((string) $manifest->maximumPhp);
-			$maximumJoomla = trim((string) $manifest->maximumJoomla);
-			
-			$this->minimumPhp = $minimumPhp ? $minimumPhp : $this->minimumPhp;
-			$this->minimumJoomla = $minimumJoomla ? $minimumJoomla : $this->minimumJoomla;
-
-			if ($maximumJoomla && version_compare(JVERSION, $maximumJoomla, '>'))
+			if ($type === 'update' || $type === 'install' || $type === 'discover_install')
 			{
-				$msg = 'Your Joomla version (' . JVERSION . ') is too high for this extension. Maximum Joomla version is: ' . $maximumJoomla . '.';
-				JLog::add($msg, JLog::WARNING, 'jerror');
-			}
-
-			// Check for the maximum PHP version before continuing
-			if ($maximumPhp && version_compare(PHP_VERSION, $maximumPhp, '>'))
-			{
-				$msg = 'Your PHP version (' . PHP_VERSION . ') is too high for this extension. Maximum PHP version is: ' . $maximumPhp . '.';
+				$minimumPhp = trim((string) $manifest->minimumPhp);
+				$minimumJoomla = trim((string) $manifest->minimumJoomla);
 				
-				JLog::add($msg, JLog::WARNING, 'jerror');
+				// Custom
+				$maximumPhp = trim((string) $manifest->maximumPhp);
+				$maximumJoomla = trim((string) $manifest->maximumJoomla);
+				
+				$this->minimumPhp = $minimumPhp ? $minimumPhp : $this->minimumPhp;
+				$this->minimumJoomla = $minimumJoomla ? $minimumJoomla : $this->minimumJoomla;
+	
+				if ($maximumJoomla && version_compare(JVERSION, $maximumJoomla, '>'))
+				{
+					$msg = 'Your Joomla version (' . JVERSION . ') is too high for this extension. Maximum Joomla version is: ' . $maximumJoomla . '.';
+					JLog::add($msg, JLog::WARNING, 'jerror');
+				}
+	
+				// Check for the maximum PHP version before continuing
+				if ($maximumPhp && version_compare(PHP_VERSION, $maximumPhp, '>'))
+				{
+					$msg = 'Your PHP version (' . PHP_VERSION . ') is too high for this extension. Maximum PHP version is: ' . $maximumPhp . '.';
+					
+					JLog::add($msg, JLog::WARNING, 'jerror');
+				}
+				
+				if (isset($msg))
+				{
+					return false;
+				}
 			}
-			
-			if (isset($msg))
-			{
-				return false;
-			}
-			
 			if (trim((string) $manifest->allowDowngrades))
 			{
 				$this->allowDowngrades = true;
 			}
 		}
+
+		if (!parent::preflight($type, $parent))
+		{
+			return false;
+		}
+
 		return true;
 	}
 
